72dec4d74256654a3b6de9fef0de85b76b8f9727
aditya-gurjar <adityaratan104@gmail.com>
2023-04-07 01:08:52 -0400

    initial commit

---
+++ entropy/Winning.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract EtheremonLite {
+    function initMonster(string memory _monsterName) public {}
+    function getName(address _monsterAddress) public view returns(string memory) {}
+    function getNumWins(address _monsterAddress) public view returns(uint) {}
+    function getNumLosses(address _monsterAddress) public view returns(uint) {}
+    function battle() public returns(bool){}
+}
+
+contract WinBattle {
+
+    // Placeholder; TODO for Q2   
+}
+
+
